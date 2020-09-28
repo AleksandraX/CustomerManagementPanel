c3d1effa1f3cbc93b9ac605b8a36918da86055ad
Bergan1 <kacper.berganski@onet.pl>
2020-09-28 20:28:52 +0200

    Create customerObjectList

--- CustomerManagementPanel/src/app/client-list/client-list.component.ts
+++ CustomerManagementPanel/src/app/client-list/client-list.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-list.component.scss']
 })
 export class ClientListComponent implements OnInit {
+
   customerList: string[] = ["Paulina Murlik" , "Sisi Murlik", "Amor Murlik"];
+  customerObjectList: Customer[] = [
+     { 
+     id: 1, 
+     name: "Kacper",
+     lastName: "Bergański", 
+     age: 21, 
+     sex: "M", 
+     city: "Chawszczyno", 
+     phoneNumber: 72123132, 
+     mail: "kacper.berganski@onet.pl"
+    },
+    new Customer(2, "Amor", "Murlik", 4, "M", "Gdynia", 312312312, "amor@onet.pl"),
+    new Customer(3, "Sisi", "Murlik", 7, "w", "Gdynia", 62438187, "sisi@wp.pl")
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -22,6 +38,39 @@ export class ClientListComponent implements OnInit {
     let index = this.customerList.indexOf(customerToDelete);    //po tym idexie usunąć pozycję z listy
     this.customerList.splice(index,1);
   }
-
-  customerToDelete:number;
+  
 }
+
+// todo przenieś do osobnej klasy
+
+export class Customer {
+  id:number;
+  name:string;
+  lastName:string;
+  age:number;
+  sex:string;
+  city:string;
+  phoneNumber:number;
+  mail:string;
+
+  constructor(
+     id: number,
+     name: string,
+     lastName:string,
+     age:number,
+     sex:string,
+     city:string,
+     phoneNumber:number,
+     mail:string) {
+      this.id = id;
+      this.name = name;
+      this.lastName = lastName;
+      this.age = age;
+      this.sex = sex;
+      this.city = city;
+      this.phoneNumber = phoneNumber;
+      this.mail = mail;
+    
+  }
+}
+
