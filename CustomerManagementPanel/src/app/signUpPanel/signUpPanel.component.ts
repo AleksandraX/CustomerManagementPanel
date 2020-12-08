a4f576d346f0cdd0cf6a98bfb591d7a7779f746e
Bergan1 <kacper.berganski@onet.pl>
2020-12-08 12:46:55 +0100

    Fixed bugs

--- CustomerManagementPanel/src/app/signUpPanel/signUpPanel.component.ts
+++ CustomerManagementPanel/src/app/signUpPanel/signUpPanel.component.ts
@@ -3,7 +3,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faEnvelope, faExclamation, faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 import { UserAccountForCreation } from '../clients/models/signUp';
 import { SignUpService } from './signUp.service';
-
 @Component({
   selector: 'app-signUpPanel',
   templateUrl: './signUpPanel.component.html',
@@ -16,17 +15,41 @@ export class SignUpPanelComponent implements OnInit {
   faEnvelope = faEnvelope;
   faUser = faUser;
   form: FormGroup = null;
-  userAccount: UserAccountForCreation = { firstName: "", lastName: "", emailAddress: "", password: ""};
+  userAccount: UserAccountForCreation = {
+    
+     firstName: "", 
+     lastName: "", 
+     emailAddress: "", 
+     password: ""
+    };
 
   constructor(
     private signUpService: SignUpService,
   ) { 
 
     this.form = new FormGroup({
-      "firstName": new FormControl(this.userAccount.firstName,[Validators.required, Validators.minLength(3), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]),
-      "lastName": new FormControl(this.userAccount.lastName,[Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern('[a-zA-Z]*')]),
-      "emailAddress": new FormControl(this.userAccount.emailAddress,[Validators.required, Validators.minLength(5), Validators.maxLength(40), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
-      "password": new FormControl(this.userAccount.password,[Validators.required, Validators.minLength(8), Validators.maxLength(40)])
+      "firstName": new FormControl(this.userAccount.firstName,[
+        Validators.required, 
+        Validators.minLength(3), 
+        Validators.maxLength(25), 
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      "lastName": new FormControl(this.userAccount.lastName,[
+        Validators.required, 
+        Validators.minLength(3), 
+        Validators.maxLength(30), 
+        Validators.pattern('[a-zA-Z]*')]),
+
+      "emailAddress": new FormControl(this.userAccount.emailAddress,[
+          Validators.required,
+          Validators.minLength(5), 
+          Validators.maxLength(40),
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+
+      "password": new FormControl(this.userAccount.password,[
+        Validators.required,
+        Validators.minLength(8), 
+        Validators.maxLength(40)])
     }); 
   }
 
@@ -44,13 +67,11 @@ export class SignUpPanelComponent implements OnInit {
     }
 
     console.log("saving", userToCreate)
-    // wywolac service i zapisac
      this.signUpService.create(userToCreate).subscribe(response => {
        console.log("Subscribe for creation")
      });
   
   };
-
 
   required(propName: string): boolean {
     return (
@@ -83,9 +104,4 @@ export class SignUpPanelComponent implements OnInit {
       this.form.get(propName).dirty
     );
     }
-
-
-
 }
-
-
